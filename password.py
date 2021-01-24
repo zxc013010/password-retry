02fc779ea589af3f8b18e98ffd8067c4fe1ef567
Bill Lin <bill0841205@gmail.com>
2021-01-24 22:10:49 +0800

    basic version

---
+++ password.py
@@ -0,0 +1,19 @@
+# 密碼重式程式
+# password = 'a123456'
+# 讓使用者重複輸入密碼
+# 最多輸入三次
+# 如果正確 印出 "登入成功"
+# 如果不正確就印出 "密碼錯誤! 還有__次機會"
+
+password = 'a123456'
+i = 3 #剩餘機會
+while True:
+	pwd = input('請輸入密碼: ')
+	if pwd == password:
+		print('登入成功')
+		break #逃出迴圈
+	else:
+		i -= 1
+		print('密碼錯誤! 還有', i, '次機會')
+		if i == 0:
+			break
