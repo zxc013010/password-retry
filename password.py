14aeedf6fea98111362b4b7040f8ca54cfa1bde7
Bill Lin <bill0841205@gmail.com>
2021-01-24 22:39:12 +0800

    finished version

--- password.py
+++ password.py
@@ -8,10 +8,14 @@
 password = 'a123456'
 i = 3 #剩餘機會
 while i > 0:
+	i -= 1
 	pwd = input('請輸入密碼: ')
 	if pwd == password:
 		print('登入成功')
 		break #逃出迴圈
 	else:
-		i -= 1
-		print('密碼錯誤! 還有', i, '次機會')
+		print('密碼錯誤!')
+		if i > 0:
+			print('密碼錯誤! 還有', i, '次機會')
+		else:
+			print('沒機會嘗試了! 鎖帳號已上鎖啦')
